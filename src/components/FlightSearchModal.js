a9562583b7617aea53437fd2691ada35cedba971
theokont <theo.kontaxis@gmail.com>
2021-10-29 00:38:31 +0300

    Add FlightSearchModal

--- src/components/FlightSearchModal.js
+++ src/components/FlightSearchModal.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import { React, useState } from 'react';
+import DatePicker from "react-datepicker";
 import { Modal, Button } from "react-bootstrap";
 import '../static/css/FlightSearchModal.css'
-
+import "react-datepicker/dist/react-datepicker.css";
 
 export default function FlightSearchModal(props) {
+    const [modalState, setModalState] = useState(0);
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+
+    function handleClose(event) {
+        props.onHide(event);
+        alert(startDate.toISOString());
+        setModalState(0);
+    }
+
     return (
         <Modal
         {...props}
@@ -19,14 +30,39 @@ export default function FlightSearchModal(props) {
                 alignItems: "center",
                 }} 
                 id="contained-modal-title-vcenter">
-                Flights for {props.cityName}, {props.countryCode}
+                Flight search for {props.cityName}
             </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-            
+        { modalState === 0 && (
+            <div>
+                <p>Please select departure and arrival date</p>
+                <div style={{display:"flex"}}>
+                    <p style={{width: "50%"}}> Departure date: </p> <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+                </div>
+                <div style={{display:"flex"}}>
+                    <p style={{width: "50%"}}> Arrival date: </p> <DatePicker selected={endDate} onChange={(date) => setEndDate(date)} />
+                </div>
+            </div>
+            )
+        }
+        { modalState === 1 && (
+            <div>
+
+            </div>   
+        )}
         </Modal.Body>
         <Modal.Footer>
-            <Button onClick={props.onHide}>Close</Button>
+            {(modalState === 0)
+            ?   <div>
+                    <Button onClick={props.onHide}>Cancel</Button>
+                    <Button style={{marginLeft:"10px"}} onClick={() => setModalState(modalState + 1)}>Search</Button>
+                </div>
+            :   <div>
+                    <Button onClick={(event) => handleClose(event)}> Close </Button>
+                </div>
+            }
+            
         </Modal.Footer>
         </Modal>
     );
